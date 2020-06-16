cd09bf544173d8a4d9157c5b7c0350bdfe184eba
warjy1226 <705282892@qq.com>
2020-06-16 17:58:30 +0800

    fix: 移除useTabel

--- type/table.d.ts
+++
@@ -1,8 +0,0 @@
-import { Ref } from '@vue/composition-api';
-
-export function useTable(uniqueId: string | number): {
-    data: Ref<[]>,
-    select: Ref<[]>,
-    selectChange(selected: any): void,
-    selectFilter(selected: any): void
-}
