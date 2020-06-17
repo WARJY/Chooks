644d6a847a8a6f17c6bc27a3cff05ba2b4d2ba22
starYd0g <gcc705282892@hotmail.com>
2020-06-17 23:54:49 +0800

    fix: 修复useSelect变量名重复bug

--- type/select.d.ts
+++ type/select.d.ts
@@ -2,7 +2,7 @@ import { Ref } from '@vue/composition-api';
 
 export function useSelect(uniqueId?:any): {
     muti: Ref<boolean>
-    select: Ref<any>
+    selected: Ref<any>
     options: Ref<Array<any>>
     select(item: any, filter?: boolean): void
     selectAll(): void
